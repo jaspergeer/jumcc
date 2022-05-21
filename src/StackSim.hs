48f74adf4870e8fb96f3c9488767dcbb3fb220d0
jaspergeer <jasper.geer@gmail.com>
2022-05-21 11:51:01 -0700

    Created stack simulation module

---
+++ src/StackSim.hs
@@ -0,0 +1,42 @@
+module StackSim where
+
+import ASTGen
+    ( CType(..),
+      Var(..),
+      Stat(WhileS, VarDeclS, IfS),
+      Param(Param))
+
+data StkFrame = StkFrame [(Var, Int)] Int deriving Show
+newtype StackSim = StackSim [StkFrame] deriving Show
+
+stkFrameEmpty = StkFrame [] 0
+stkSimEmpty = StackSim []
+
+stkSimQuery :: StackSim -> Var-> Int
+stkSimQuery (StackSim stk) = _ssq stk where
+    _ssq [] (Var n) = error $ "couldn't recognize symbol " ++ n
+    _ssq (x@(StkFrame entries size):xs) v = case lookup v entries of
+        Just n -> n
+        Nothing -> stkSimQuery (StackSim xs) v
+
+stkSimPushFrame :: StackSim -> StackSim
+stkSimPushFrame (StackSim stk) = StackSim (stkFrameEmpty:stk)
+
+stkSimPopFrame :: StackSim -> StackSim
+stkSimPopFrame (StackSim (x:xs)) = StackSim xs
+stkSimPopFrame (StackSim []) = error "can't pop frame: stack empty"
+
+stkSimPush :: StackSim -> (Var, CType) -> StackSim
+stkSimPush (StackSim (f:fs)) x = StackSim (_ssp f x:fs) where
+    _ssp (StkFrame f s) (v, typ) = StkFrame ((v, 0):map (\(x,y) -> (x, y + typeSize typ)) f) (s + typeSize typ)
+stkSimPush (StackSim []) _ = error "can't push value: stack empty" 
+
+stkSimPop :: StackSim -> StackSim
+stkSimPop (StackSim (f:fs)) = StackSim fs
+stkSimPop (StackSim []) = error "can't pop value: stack empty"
+
+typeSize :: CType -> Int
+typeSize (Ptr _) = 1
+typeSize CInt = 1
+typeSize CChar = 1
+typeSize (Arr n typ) = n * typeSize typ
