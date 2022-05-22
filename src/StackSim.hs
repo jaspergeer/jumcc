5dd04de029f34c0b5f72b0fc7e530c7d169b48f0
jaspergeer <jasper.geer@gmail.com>
2022-05-22 10:07:10 -0700

    Fixed bug where offsets would not update on pop

--- src/StackSim.hs
+++ src/StackSim.hs
@@ -6,11 +6,8 @@ import ASTGen
       Stat(WhileS, VarDeclS, IfS),
       Param(Param))
 
-data StkFrame = StkFrame [(Var, Int)] Int deriving Show
+data StkFrame = StkFrame String [(Var, Int)] Int deriving Show
 newtype StackSim = StackSim [StkFrame] deriving Show
-
-stkFrameEmpty :: StkFrame
-stkFrameEmpty = StkFrame [] 0
 
 stkSimEmpty :: StackSim
 stkSimEmpty = StackSim []
@@ -18,24 +15,26 @@ stkSimEmpty = StackSim []
 stkSimQuery :: StackSim -> Var-> Int
 stkSimQuery (StackSim stk) = _ssq stk where
     _ssq [] (Var n) = error $ "couldn't recognize symbol " ++ n
-    _ssq (x@(StkFrame entries size):xs) v = case lookup v entries of
+    _ssq (x@(StkFrame _ entries size):xs) v = case lookup v entries of
         Just n -> n
         Nothing -> stkSimQuery (StackSim xs) v + size
 
-stkSimPushFrame :: StackSim -> StackSim
-stkSimPushFrame (StackSim stk) = StackSim (stkFrameEmpty:stk)
+stkSimPushFrame :: StackSim -> String -> StackSim
+stkSimPushFrame (StackSim stk) n = StackSim (StkFrame n [] 0:stk)
 
 stkSimPopFrame :: StackSim -> StackSim
 stkSimPopFrame (StackSim (x:xs)) = StackSim xs
 stkSimPopFrame (StackSim []) = error "can't pop frame: stack empty"
 
 stkSimPush :: StackSim -> (Var, CType) -> StackSim
-stkSimPush (StackSim (f:fs)) x = StackSim (_ssp f x:fs) where
-    _ssp (StkFrame f s) (v, typ) = StkFrame ((v, 0):map (\(x,y) -> (x, y + typeSize typ)) f) (s + typeSize typ)
+stkSimPush (StackSim (f:fs)) x = StackSim (_spush f x:fs) where
+    _spush (StkFrame n f s) (v, typ) = StkFrame n ((v, 0):map (\(x,y) -> (x, y + typeSize typ)) f) (s + typeSize typ)
 stkSimPush (StackSim []) _ = error "can't push value: stack empty"
 
 stkSimPop :: StackSim -> StackSim
-stkSimPop (StackSim ((StkFrame (f:fs) s):stk)) = StackSim (StkFrame fs s:stk)
+stkSimPop (StackSim ((StkFrame n (f:fs) s):stk)) = StackSim (StkFrame n (_spop fs) (s - 1):stk) where
+    _spop ((x, y):xs) = (x, y - 1):_spop xs
+    _spop [] = []
 stkSimPop (StackSim _) = error "can't pop value: stack empty"
 
 typeSize :: CType -> Int
