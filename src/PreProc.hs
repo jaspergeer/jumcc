281dea97e9479e9700300497889c10b6b35be769
jaspergeer <jasper.geer@gmail.com>
2022-05-24 17:34:13 -0700

    Removing comments now preserves endline characters

--- src/PreProc.hs
+++ src/PreProc.hs
@@ -2,11 +2,12 @@ module PreProc where
 
 remComments :: String -> String -> String
 remComments x ('/':'/':ys) = _slcomm x ys where
-    _slcomm x ('\n':ys) = remComments x ys
+    _slcomm x ('\n':ys) = remComments (x ++ "\n") ys
     _slcomm x (y:ys) = _slcomm x ys
     _slcomm x [] = x
 remComments x ('/':'*':ys) = _mlcomm x ys where
     _mlcomm x ('*':'/':ys) = remComments x ys
+    _mlcomm x ('\n':ys) = _mlcomm (x ++ "\n") ys
     _mlcomm x (y:ys) = _mlcomm x ys
     _mlcomm x [] = x
 remComments x [] = x
