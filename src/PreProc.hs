1b05b26ef33064fab725072844875580f9ac88c5
Jasper Yujin Geer <jasper.geer@gmail.com>
2022-06-29 22:09:00 -0700

    Added include support, source position directives

--- src/PreProc.hs
+++ src/PreProc.hs
@@ -6,37 +6,53 @@
  -}
 
 module PreProc where
-import GHC.IO.FD (openFile)
-import GHC.IO.Handle (hGetContents)
-import GHC.IO.IOMode (IOMode(ReadMode))
+import Text.Parsec
+    ( char,
+      string,
+      anyChar,
+      spaces,
+      between,
+      manyTill,
+      (<|>),
+      getPosition,
+      many,
+      setPosition,
+      try, SourcePos, sourceName, sourceLine, sourceColumn, noneOf, parse )
+import Text.Parsec.String (Parser)
+import Data.Foldable (find)
+import Text.Parsec.Pos (newPos, initialPos)
 
-preProc :: String -> [(String, String)] -> String
-preProc src headers = remPreProcDirs (insertIncludes src "" headers) ""
+preProc :: String -> [(String, String)] -> Parser String
+preProc srcName headers = do
+    setPosition $ initialPos srcName
+    result <- many (try singleLineComment
+                                    <|> try (spaces *> multiLineComment <* spaces)
+                                    <|> try (spaces *> include headers <* spaces)
+                                    <|> try (do
+                                        c <- anyChar
+                                        return [c]))
+    return $ concat result
 
-remPreProcDirs :: String -> String -> String
-remPreProcDirs x ('/':'/':ys) = _slcomm x ys where
-    _slcomm x ('\n':ys) = remPreProcDirs (x ++ "\n") ys
-    _slcomm x (y:ys) = _slcomm x ys
-    _slcomm x [] = x
-remPreProcDirs x ('#':ys) = _preprocdir x ys where
-    _preprocdir x ('\n':ys) = remPreProcDirs (x ++ "\n") ys
-    _preprocdir x (y:ys) = _preprocdir x ys
-    _preprocdir x [] = x
-remPreProcDirs x ('/':'*':ys) = _mlcomm x ys where
-    _mlcomm x ('*':'/':ys) = remPreProcDirs x ys
-    _mlcomm x ('\n':ys) = _mlcomm (x ++ "\n") ys
-    _mlcomm x (y:ys) = _mlcomm x ys
-    _mlcomm x [] = x
-remPreProcDirs x [] = x
-remPreProcDirs x [y] = x ++ [y]
-remPreProcDirs x (y:ys) = remPreProcDirs (x ++ [y]) ys
+singleLineComment :: Parser String
+singleLineComment = do
+    string "//" <* manyTill anyChar (try $ char '\n')
+    sourcePosDirectiveFrom <$> getPosition
 
-insertIncludes :: String -> String -> [(String, String)] -> String
-insertIncludes ('#':'i':'n':'c':'l':'u':'d':'e':' ':'\"':xs) result headers = _insert xs result headers "" where
-    _insert ('\"':xs) result headers name = insertIncludes xs (result ++ _lookup name headers) headers
-    _insert (x:xs) result headers name = _insert xs result headers (name ++ [x])
-    _insert [] _ _ _ = error "name of file not followed by \""
-    _lookup q ((k,v):xs) = if k == q then v else _lookup q xs
-    _lookup q _ = error ("file \"" ++ q ++ "\" not found")
-insertIncludes (x:xs) result headers = insertIncludes xs (result ++ [x]) headers
-insertIncludes [] result headers = result
+multiLineComment :: Parser String
+multiLineComment = do
+    string "/*" <* manyTill anyChar (try $ string "*/" <* spaces)
+    sourcePosDirectiveFrom <$> getPosition
+
+include :: [(String, String)] -> Parser String
+include headers = do
+    key <- string "#include" *> spaces *> between (char '"') (char '"') (many (noneOf "\"")) <* spaces
+    pos1 <- getPosition
+    text <- (case find (\(x, y) -> x == key) headers of
+        Nothing -> error (key ++ ": no such file or directory")
+        Just (_, y) -> case parse (preProc key headers) "" y of
+            Right a -> return a
+            Left b -> error (show b))
+    return $ sourcePosDirectiveFrom (initialPos key) ++ text ++ sourcePosDirectiveFrom pos1
+
+sourcePosDirectiveFrom :: SourcePos -> String
+sourcePosDirectiveFrom x = "\n!source_pos \"" ++ sourceName x ++ "\" " ++ show (sourceLine x) ++ " " ++ show (sourceColumn x) ++ "\n"
