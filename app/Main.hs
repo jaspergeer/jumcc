4473b3d2b939a44c7133a52e49b34e9b6e9a65c6
Jasper Yujin Geer <jasper.geer@gmail.com>
2022-07-05 11:47:47 -0700

    Input is now parsed, type checked, then lowered to umasm

--- app/Main.hs
+++ app/Main.hs
@@ -6,46 +6,57 @@
  -}
 
 module Main where
-import ASTGen ( program )
-import AsmGen ( visitProgram, AsmProg(..) )
+import CodeGen ( AsmProg(..), visitAST )
+import ASTGen (annAST)
 import StackSim ()
 import Text.Parsec.String (Parser)
-import Text.Parsec.Char ()
 import Text.Parsec ( ParseError, parse )
 import System.Environment (getArgs)
 import System.IO (openFile, IOMode(ReadMode), hGetContents)
 import Text.Parsec.Error (errorMessages, messageString)
-import PreProc (remComments)
+import PreProc (preProc)
 import GHC.Base (IO(IO))
+import GHC.IO.Handle (hGetContents)
+import DeAnn (deAnnotate)
+import TypeChecker (typeCheck)
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        ("-o":out:src:xs) -> case reverse src of
+        ("-o":outName:inName:xs) -> do
+            result <- compile inName xs
+            printAsm result outName
+        (inName:xs) -> do
+            result <- compile inName xs
+            printAsm result "out.ums"
+        _ -> putStrLn "Usage: jumcc [-o OUT-FILE] IN-FILE ..."
+
+compile :: String -> [String] -> IO AsmProg
+compile inName headerNames = case reverse inName of
             ('c':'m':'u':'.':xs) -> do
-                file <- openFile src ReadMode
-                text <- hGetContents file
-                printAsm (compile (reverse xs) (remComments "" text)) out
+                file <- openFile inName ReadMode
+                src <- hGetContents file
+                headers <- getHeaders headerNames
+                preProcd <- (case parse (preProc inName headers) "" src of
+                    Right a -> return a
+                    Left b -> error (show b))
+                aAST <- (case parse annAST "" preProcd of
+                    Left a -> error (show a)
+                    Right b -> return b)
+                ast <- (case typeCheck aAST of
+                    Just a -> error (show a)
+                    Nothing -> pure $ deAnnotate aAST)
+                return $ visitAST ast inName
             _ -> error "source files should have suffix .umc"
-        (src:xs) -> case reverse src of
-            ('c':'m':'u':'.':xs) -> do
-                file <- openFile src ReadMode
-                text <- hGetContents file 
-                printAsm (compile (reverse xs) (remComments "" text)) (reverse xs ++ ".ums")
-            _ -> error "source files should have suffix .umc"
-        _ -> putStrLn "Usage: jumcc [-o OUT-FILE] IN-FILE"
 
-compile :: String -> String -> AsmProg
-compile pname text = case parse program "" text of
-    Right ast -> visitProgram ast pname
-    Left err -> error (show err)
+getHeaders :: [String] -> IO [(String, String)]
+getHeaders (x:xs) = do
+    file <- openFile x ReadMode
+    src <- hGetContents file
+    rest <- getHeaders xs
+    return $ (x, src) : rest
+getHeaders [] = return []
 
-headerList :: [String] -> [String]
-headerList (x:xs) = do
-    contents hGetContents <$> openFile x ReadMode of
-    (IO c) -> [c]
-
-headerList [] = []
 printAsm :: AsmProg -> String -> IO()
 printAsm (AsmProg _ asm _ _) fname = writeFile fname $ foldl (\x y ->  x ++ (if (head y == '.') || (last y == ':') then "" else "    ") ++ y ++ "\n") "" asm ++ "\n"
